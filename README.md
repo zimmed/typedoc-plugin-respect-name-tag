cd08cd1f99bed8b0e78e5209b61c1836af0a3ae1
Sebastián Gurin <sebastigurin@gmail.com>
2018-04-23 11:51:28 -0300

    1.0.2

--- README.md
+++ README.md
@@ -15,19 +15,19 @@ the event like the method, `addListener`. The method signature will still be use
 for the event, the only thing that change is its name. 
 
 ```ts
-interface Cart {
- /**
-  * Register given listener function to be notified when the user add the items to the cart 
-  * @event
-  * @name before:add-to-cart
-  * @param listener accepts the items that the user had intention to add to the cart and 
-  * a promise that will be resolved when the transaction is fulfilled or rejected 
-  * otherwise. Also the listener have the possibility to asynchronously validate 
-  * the transaction yb returning a promise. If so the transaction won't start 
-  * unless the promise is resolved (could be useful to validate with third parties 
-  * providers)
-  */
-  addListener(listener:(items:IItem[], transaction:Promise<Transaction>)=>Promise<boolean>):void
+export interface Cart {
+  /**
+   * Register given listener function to be notified when the user add the items to the cart 
+   * @event
+   * @name before:add-to-cart
+   * @param listener accepts the items that the user had intention to add to the cart and 
+   * a promise that will be resolved when the transaction is fulfilled or rejected 
+   * otherwise. Also the listener have the possibility to asynchronously validate 
+   * the transaction yb returning a promise. If so the transaction won't start 
+   * unless the promise is resolved (could be useful to validate with third parties 
+   * providers)
+   */
+  addListener(listener:(items:IItem[], transaction:Promise<ITransaction>) => Promise<boolean>):void;
 }
 ```
 
--- plugin.ts
+++ plugin.ts
@@ -3,28 +3,35 @@ import { Converter } from 'typedoc/dist/lib/converter/converter';
 import { Reflection } from 'typedoc/dist/lib/models/reflections/abstract';
 import { Context } from 'typedoc/dist/lib/converter/context';
 /**
- * This plugin will force TypeDoc to use the name declared in @name annotation. For 
+ * This plugin will force TypeDoc to use the name declared in &#64;name annotation. For 
  * example, the following class declares a an event member named `before:add-to-cart` 
  * although the associated node is a method with the name `addListener`. The method 
  * signature will still be used for the event, i.e. the callback function signature: 
  * 
  *  @example
  * ```ts
- * TODO: check this code! and add missing sample documentation for params promise , return, etc
- * interface Cart {
- *  &#47;**
- *   * Register given listener function to be notified when the user add the items to the cart 
- *   * &#64;event
- *   * &#64;name before:add-to-cart
- *   * &#64;param listener
- *   *&#47;
- *  addListener(listener:(items:IItem[], transaction:Promise<Transaction>)=>Promise<boolean>):void
+ * export interface Cart {
+ *   &#47;**
+ *    * Register given listener function to be notified when the user add the items to the cart 
+ *    * &#64;event
+ *    * &#64;name before:add-to-cart
+ *    * &#64;param listener accepts the items that the user had intention to add to the cart and 
+ *    * a promise that will be resolved when the transaction is fulfilled or rejected 
+ *    * otherwise. Also the listener have the possibility to asynchronously validate 
+ *    * the transaction yb returning a promise. If so the transaction won't start 
+ *    * unless the promise is resolved (could be useful to validate with third parties 
+ *    * providers)
+ *    *&#47;
+ *   addListener(listener:(items:IItem[], transaction:Promise<ITransaction>) => Promise<boolean>):void;
  * }
  * ```
+ * 
  */
 @Component({ name: 'respect-name-tag' })
 export class RespectNameTagPlugin extends ConverterComponent {
-  private respectThisNames:RespectNameTagRename[]
+
+  private respectThisNames:RespectNameTagRename[];
+  
   initialize() {
     this.listenTo(this.owner, {
       [Converter.EVENT_BEGIN]: this.onBegin,
@@ -50,17 +57,17 @@ export class RespectNameTagPlugin extends ConverterComponent {
    * @param node  The node that is currently processed if available.
    */
   private onDeclaration(context: Context, reflection: Reflection, node?) {
-    if(node.symbol && node.jsDoc){
-      let tags =[]
-      node.jsDoc.forEach(node=>
+    if (node.symbol && node.jsDoc) {
+      let tags = [];
+      node.jsDoc.forEach(node =>
         tags = tags.concat(
-          (node.tags||[])
-          .filter(tag=>tag.tagName && tag.tagName.text=='name')
-        )
+          (node.tags || [])
+          .filter(tag => tag.tagName && tag.tagName.text === 'name'),
+        ),
       );
-      if(tags.length){
+      if (tags.length) {
         // TODO. what if tags[0].length>1 ? that could mean user write two @name tags - we are using the last one
-        this.respectThisNames.push({renameTo: tags[tags.length-1].comment, reflection})
+        this.respectThisNames.push({ renameTo: tags[tags.length - 1].comment, reflection });
       }
     }
   }
@@ -71,9 +78,9 @@ export class RespectNameTagPlugin extends ConverterComponent {
    * @param context  The context object describing the current state the converter is in.
    */
   private onBeginResolve(context: Context) {
-    this.respectThisNames.forEach(item=>{
-      item.reflection.name = item.renameTo
-    })
+    this.respectThisNames.forEach(item => {
+      item.reflection.name = item.renameTo;
+    });
   }
 }
 
@@ -84,6 +91,6 @@ interface RespectNameTagRename {
 
 // Finally we export the plugin class as a TypeDoc Plugin - this is we register it as a 
 // TypeDoc component with our name 'respect-name-tag'
-export default function(PluginHost) {
+export default function (PluginHost) {
   PluginHost.owner.converter.addComponent('respect-name-tag', RespectNameTagPlugin);
-};
+}
